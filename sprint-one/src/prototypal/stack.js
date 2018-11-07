76214d964e716ca01d05d54bd03a1017b5732d70
klarok <karaylu@gmail.com>
2018-11-07 15:08:57 -0800

    Prototypal stack passes test suite.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let stackObj = Object.create(stackMethods);
+  stackObj.storage = {};
+
+  return stackObj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this.storage[this.size()] = value;
+	},
+	pop: function() {
+		let last = this.size() - 1;
+		let popped = this.storage[last];
+		delete this.storage[last];
+		return popped;
+	},
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
