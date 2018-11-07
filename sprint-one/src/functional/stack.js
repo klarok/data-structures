b381ac8c84529ddcccc94a4854a24debcd573da9
klarok <karaylu@gmail.com>
2018-11-07 11:55:59 -0800

    Functional pattern stack passes test suite.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = Object.keys(storage).length;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size++] = value; //Add value and increment size (after assigning appropriate key)
   };
 
   someInstance.pop = function() {
+    let popped = storage[--size]; //Decrement size, get the last object
+    delete storage[size];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
