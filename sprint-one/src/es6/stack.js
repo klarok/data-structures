9b3a89d3de6a033a39d443b618234a8cf692317f
klarok <karaylu@gmail.com>
2018-11-07 19:53:53 -0800

    ES6 stack passes test suite.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  	this.length = 0;
+  }
+
+  push(value) {
+  	this.storage[this.length] = value;
+  	this.length += 1;
+  }
+
+  pop() {
+  	let last = this.length - 1;
+  	let popped = this.storage[last];
+  	delete this.storage[last];
+  	this.length = Math.max(last, 0);
+  	return popped;
+  }
+
+  size() {
+  	return this.length;
   }
 
 }
