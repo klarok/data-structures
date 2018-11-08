18f37bf16486e2fdcc4497d42ca706261c1af7dc
klarok <karaylu@gmail.com>
2018-11-07 19:58:30 -0800

    ES6 stack edited to conform with style guide. ES6 queue passes test suite.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
+    this.head = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.head + this.length] = value;
+    this.length += 1;
+  }
+
+  dequeue() {
+    let first = this.head;
+    let dequeued = this.storage[first];
+    delete this.storage[first];
+    this.length = Math.max(this.length - 1, 0);
+    this.head = (this.length === 0) ? 0 : this.head + 1;
+    return dequeued;
+  }
+
+  size() {
+    return this.length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,25 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  	this.storage = {};
-  	this.length = 0;
+    this.storage = {};
+    this.length = 0;
   }
 
   push(value) {
-  	this.storage[this.length] = value;
-  	this.length += 1;
+    this.storage[this.length] = value;
+    this.length += 1;
   }
 
   pop() {
-  	let last = this.length - 1;
-  	let popped = this.storage[last];
-  	delete this.storage[last];
-  	this.length = Math.max(last, 0);
-  	return popped;
+    let last = this.length - 1;
+    let popped = this.storage[last];
+    delete this.storage[last];
+    this.length = Math.max(last, 0);
+    return popped;
   }
 
   size() {
-  	return this.length;
+    return this.length;
   }
 
 }
