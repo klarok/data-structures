1b810ddbb71586217a036076ba3848cb6e2a5152
klarok <karaylu@gmail.com>
2018-11-08 17:44:22 -0800

    Finsihed refactored tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,44 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree,treeMethods);
+  newTree.children = []; 
+    
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  // fix me
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+  console.log('Tree ' + this.value + ' added child');
+  console.log(this.children);
+  
 };
 
 treeMethods.contains = function(target) {
+  let isThere = false;
+  if(this.value === target){
+    return true;
+    
+  } else if(this.children.length === 0){
+      return false;
+      
+  } else {
+    //this.children.forEach(function(child) {
+      //isThere = isThere || child.contains(target);
+    //});
+    for(var i = 0;i<this.children.length;i++){
+      isThere = this.children[i].contains(target);
+      if(isThere){
+        break;
+      }
+    }
+  }
+  return isThere;
 };
 
 
