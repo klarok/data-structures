69f4ad22c0c8dee0a67f06dd555ed532df163f18
klarok <karaylu@gmail.com>
2018-11-09 10:51:05 -0800

    Implemented graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -17,12 +17,16 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  delete this.nodes[node]
+  let removeNeighbors = this.nodes[node];
+  for (var i = 0; i < removeNeighbors.length; i++) {
+    this.removeEdge(node, removeNeighbors[i]);
+  }
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-   return this.nodes[fromNode].includes(toNode)
+  return this.nodes[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
@@ -33,18 +37,27 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  let currNode = this.nodes[JSON.stringify(fromNode)];
-  for(var i=0;i<currNode.length;i++){
-    if(currNode[i]=== toNode){
-      currNode.splice(i,1);
-      this.removeEdge(toNode,fromNode)
-      //Find toNode's list of edges and remove fromNode
+  let fNode = this.nodes[JSON.stringify(fromNode)];
+  let tNode = this.nodes[JSON.stringify(toNode)];
+  for (var i = 0; i < fNode.length; i++) {
+    if (fNode[i] === toNode) {
+      fNode.splice(i, 1);
+      for (var j = 0; j < tNode.length; j++) {
+        if (tNode[j] === fromNode) {
+          tNode.splice(j, 1);
+          break;
+        }
+      }
+      break;
     }
   }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let key in this.nodes) {
+    cb(Number(key));
+  }
 };
 
 /*
