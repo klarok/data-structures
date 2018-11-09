c02304b007a3523de24b4533b6b6843e7923be7c
klarok <karaylu@gmail.com>
2018-11-09 15:23:25 -0800

    Styled Tree.js( remove console.log statements)and Implemented Hash Table with arrays

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,48 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  if (bucket === undefined) {
+    bucket = [];
+    this._storage.set(index, bucket);
+    
+  }
+  
+  let existingIndex = this.findkey(bucket, k);
+  if (existingIndex === -1) {
+    bucket.push([k, v]);
+  } else {
+    bucket[existingIndex][1] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  // for (var i = 0; i < bucket.length; i++ ) {
+  //   if (bucket[i][0] === k) {
+  //     return bucket[i][1];
+  //   }
+  // }
+  let existingIndex = this.findkey(bucket, k);
+  return (existingIndex !== -1) ? bucket[existingIndex][1] : undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  let existingIndex = this.findkey(bucket, k);
+  bucket.splice(existingIndex, 1);
+};
+
+HashTable.prototype.findkey = function(bucket, k) {
+  let existingIndex = -1;
+  for (let i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      existingIndex = i;
+    }
+  }
+  return existingIndex;
 };
 
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -14,26 +14,26 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   this.children.push(Tree(value));
-  console.log('Tree ' + this.value + ' added child');
-  console.log(this.children);
+  //console.log('Tree ' + this.value + ' added child');
+  //console.log(this.children);
   
 };
 
 treeMethods.contains = function(target) {
   let isThere = false;
-  if(this.value === target){
+  if (this.value === target) {
     return true;
     
-  } else if(this.children.length === 0){
-      return false;
+  } else if (this.children.length === 0) {
+    return false;
       
   } else {
     //this.children.forEach(function(child) {
-      //isThere = isThere || child.contains(target);
+    //isThere = isThere || child.contains(target);
     //});
-    for(var i = 0;i<this.children.length;i++){
+    for (var i = 0; i < this.children.length; i++) { 
       isThere = this.children[i].contains(target);
-      if(isThere){
+      if (isThere) {
         break;
       }
     }
